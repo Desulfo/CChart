fe95801677e8e7fb5c9437c05d3cde12780b8569
desulfo <puyol_11@interia.pl>
2021-07-30 16:28:23 +0200

    add chart

--- src/pages/App.js
+++ src/pages/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 import './App.css';
 import Form from '../molecules/From'
+import Chart from '../molecules/Chart'
 
 function App() {
   const [data, setData] =useState([null, null]);
@@ -16,6 +17,7 @@ function App() {
       <h1>Hello</h1>
       <Form setData={setData}/>
       {list}
+      <Chart />
     </>
   );
 }
