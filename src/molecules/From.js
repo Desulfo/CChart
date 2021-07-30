86032105ae556ed93e5b6d961e7490969c29b383
desulfo <puyol_11@interia.pl>
2021-07-30 16:37:37 +0200

    correct selected country value

--- src/molecules/From.js
+++ src/molecules/From.js
@@ -1,35 +1,46 @@
-import React, {useState} from 'react'
-import './form.css'
+import React, { useState } from "react";
+import "./form.css";
 
 const today = new Date();
 today.setDate(today.getDate());
-const date = today.toISOString().substr(0,10);
+const date = today.toISOString().substr(0, 10);
 
+function From({ setData }) {
+  const initMonth = "DEU";
+  const [month, setMonth] = useState(initMonth);
+  const [country, setCountry] = useState(date);
 
-function From( {setData} ) {
-    const initMonth = "DE";
-    const [month, setMonth] = useState(initMonth);
-    const [country, setCountry] = useState(date);
+  const changeData = (e) => {
+    e.preventDefault();
+    setData((prevState) => [month, country]);
+  };
 
-    const changeData = (e) => {
-        e.preventDefault();
-        setData( prevState => [month, country])
-    }
-
-    return (
-        <form>
-            <label>month 
-                <input id="month" type="date" defaultValue={date} onChange={(e)=> setMonth(e.target.value)}/> 
-                </label>
-            <label>country 
-                <select id="country" defaultValue={initMonth} list="country" onChange={(e)=> setCountry(e.target.value)}>
-                <option value="PL">Poland</option> 
-                <option value="DE">Germany</option>
-                </select>
-            </label>
-            <input onClick={changeData} type="submit"/>
-        </form>
-    )
+  return (
+    <form>
+      <label>
+        month
+        <input
+          id="month"
+          type="date"
+          defaultValue={date}
+          onChange={(e) => setMonth(e.target.value)}
+        />
+      </label>
+      <label>
+        country
+        <select
+          id="country"
+          defaultValue={initMonth}
+          list="country"
+          onChange={(e) => setCountry(e.target.value)}
+        >
+          <option value="POL">Poland</option>
+          <option value="DEU">Germany</option>
+        </select>
+      </label>
+      <input onClick={changeData} type="submit" />
+    </form>
+  );
 }
 
-export default From
+export default From;
