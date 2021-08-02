44907137b759b000fe10c76c77e3a4c91bf480d2
desulfo <puyol_11@interia.pl>
2021-08-02 17:20:23 +0200

    add fetching data

---
+++ src/molecules/Form.js
@@ -0,0 +1,40 @@
+import React, { useRef } from "react";
+import classes from "./form.module.css";
+
+const today = new Date();
+today.setDate(today.getDate());
+const date = today.toISOString().substr(0, 10);
+
+const initCountry = "DEU";
+
+function Form({ setFetchData }) {
+  const dateRef = useRef();
+  const countryRef = useRef();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setFetchData({
+      date: dateRef.current.value,
+      country: countryRef.current.value,
+    });
+  };
+  return (
+    <form onSubmit={submitHandler}>
+      <fieldset>
+        <label>
+          month
+          <input ref={dateRef} type="date" defaultValue={date} />
+        </label>
+        <label>
+          country
+          <select ref={countryRef} defaultValue={initCountry} list="country">
+            <option value="POL">Poland</option>
+            <option value="DEU">Germany</option>
+          </select>
+        </label>
+      </fieldset>
+      <input type="submit" />
+    </form>
+  );
+}
+
+export default Form;
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,23 +1,59 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer, useEffect } from "react";
 
-import './App.css';
-import Form from '../molecules/From'
-import Chart from '../molecules/Chart'
+import classes from "./App.module.css";
+import Form from "../molecules/Form";
+import Chart from "../molecules/Chart";
+
+const endpoint = (country, startingDate, endingDate) => {
+  return `https://covidapi.info/api/v1/country/${country}/timeseries/${startingDate}/${endingDate}`;
+};
+
+const fetchDataReducer = (state, action) => {
+  return {
+    date: action.date,
+    country: action.country,
+  };
+};
+const formatDate = (date) => {
+  var d = new Date(date),
+    month = "" + (d.getMonth() + 1),
+    day = "" + d.getDate(),
+    year = d.getFullYear();
+
+  if (month.length < 2) month = "0" + month;
+  if (day.length < 2) day = "0" + day;
+
+  return [year, month, day].join("-");
+};
 
 function App() {
-  const [data, setData] =useState([null, null]);
-  let list;
-  if(data[0]){
-    list=  <ul>
-      {data.map(item => <li key={item}>{item}</li>)}
-    </ul>
-  } 
+  const [fetchData, fetchDataDispatch] = useReducer(fetchDataReducer, {
+    date: "",
+    country: "",
+  });
+  const [results, setResults] = useState({});
+
+  useEffect(() => {
+    const inputData = new Date(fetchData.date);
+    inputData.setMonth(inputData.getMonth() + 1); //month later
+    const startingDate = formatDate(fetchData.date);
+    const endingDate = formatDate(inputData);
+    console.log(endpoint(fetchData.country, startingDate, endingDate));
+    console.log(
+      "https://covidapi.info/api/v1/country/IND/timeseries/2020-03-15/2020-03-20"
+    );
+    fetch(endpoint(fetchData.country, startingDate, endingDate))
+      .then((response) => response.json())
+      .then((data) => setResults(data["result"]));
+    // return () => {
+    //   cleanup
+    // }
+  }, [fetchData]);
   return (
     <>
-      <h1>Hello</h1>
-      <Form setData={setData}/>
-      {list}
-      <Chart />
+      <h1 className={classes.mainHeader}>Covid chart</h1>
+      <Form setFetchData={fetchDataDispatch} />
+      <Chart results={results} />
     </>
   );
 }
